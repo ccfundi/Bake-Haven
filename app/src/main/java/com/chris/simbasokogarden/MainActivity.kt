e05bf3e12c375fa081157a80139d43e11c46994d
ccfundi <ccfundi001@gmail.com>
2026-04-10 13:43:10 +0300

    This is an android project named Bake Haven

---
+++ app/src/main/java/com/chris/simbasokogarden/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.chris.simbasokogarden
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+//        Sign Up intent
+        val signup=findViewById<Button>(R.id.signup)
+
+//        Set on click listener
+        signup.setOnClickListener {
+//            Enable our intent or do our navigation
+
+            val signupIntent = Intent(applicationContext, SignUp::class.java)
+            startActivity(signupIntent)
+
+        }
+
+//        Sign In intent
+        val signin=findViewById<Button>(R.id.signin)
+
+//        set on click listener
+        signin.setOnClickListener {
+
+            val signinIntent=Intent(applicationContext,SignIn::class.java)
+            startActivity(signinIntent)
+        }
+
+
+
+
+    }
+}
